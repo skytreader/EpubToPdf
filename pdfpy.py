114887769dcfc8694ed53709e33dc26774b1505f
Chad Estioco <chadestioco@gmail.com>
2021-12-26 20:43:29 +0800

    Whitespace clean-up.

--- pdfpy.py
+++ pdfpy.py
@@ -6,55 +6,55 @@ from PyPDF2.utils import PdfReadError
 
 class PdfEngine(object):
 
-	"""
-		This class carries operations on pdf files.
+    """
+        This class carries operations on pdf files.
 
-		It has the following methods:
+        It has the following methods:
 
-		convert() --- Which converts each of the markup file
-		passed in to pdf. Markup file should be html
+        convert() --- Which converts each of the markup file
+        passed in to pdf. Markup file should be html
 
-		combine() --- Which merges all of the pdf files created by
-		the convert method, creating a new file.
+        combine() --- Which merges all of the pdf files created by
+        the convert method, creating a new file.
 
-		del_pdf() --- Which deletes all the pdf files created by
-		the convert method.
+        del_pdf() --- Which deletes all the pdf files created by
+        the convert method.
 
-	"""
+    """
 
-	def __init__(self, markup_files, style_files, pdf_files, directory):
-		self.markup_files = markup_files
-		self.style_files = style_files
-		self.pdf_files = pdf_files
-		self.directory = directory
+    def __init__(self, markup_files, style_files, pdf_files, directory):
+        self.markup_files = markup_files
+        self.style_files = style_files
+        self.pdf_files = pdf_files
+        self.directory = directory
 
-	def convert(self):
-		for each in self.markup_files:
+    def convert(self):
+        for each in self.markup_files:
 
-			# Prevent conversion process from showing terminal updates
-			options = {"enable-local-file-access": None, "quiet": ""}
-			pdfkit.from_file(each, "{}.pdf".format(self.markup_files.index(each)),
-							 options=options)
+            # Prevent conversion process from showing terminal updates
+            options = {"enable-local-file-access": None, "quiet": ""}
+            pdfkit.from_file(each, "{}.pdf".format(self.markup_files.index(each)),
+                             options=options)
 
-		print('--- Sections converted to pdf')
+        print('--- Sections converted to pdf')
 
-	def combine(self):
+    def combine(self):
 
-		merger = PdfFileMerger()
+        merger = PdfFileMerger()
 
-		for pdf in self.pdf_files:
-			try:
-				merger.append(pdf, import_bookmarks=False)
-			except PdfReadError:
-				pass
+        for pdf in self.pdf_files:
+            try:
+                merger.append(pdf, import_bookmarks=False)
+            except PdfReadError:
+                pass
 
-		merger.write("{}.pdf".format(self.directory))
+        merger.write("{}.pdf".format(self.directory))
 
-		print('--- Sections combined together in a single pdf file')
+        print('--- Sections combined together in a single pdf file')
 
-		merger.close()
+        merger.close()
 
-	def del_pdf(self):
-			for each in self.pdf_files:
-				os.remove(each)
-			print('--- Individual pdf files deleted from directory')
+    def del_pdf(self):
+            for each in self.pdf_files:
+                os.remove(each)
+            print('--- Individual pdf files deleted from directory')
